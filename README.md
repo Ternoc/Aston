13584b43e253dc1dcbcbbbea73c3104bf51d64fb
sccl <contact@sccl.me>
2026-05-04 17:45:10 +0200

    Mise à jour avec Pandas

---
+++ README.md
@@ -0,0 +1,8 @@
+Ce script pyhton a été réalisé dans le cadre de la Licence de Physique de l'université de Bordeaux.
+Il correspond à un exercice de l'UE Physique Subatomique animé par Mathias Gerbaux.
+
+En 2026, il a été mis à jour afin de remplacer la lecture du fichier de données par expression régulière avec une lecture par pandas.
+
+La courbe d'Aston est, en physique nucléaire, une courbe représentant l'énergie de liaison par nucléon des noyaux atomiques, en fonction de leur nombre de masse (source [Wikipedia](https://fr.wikipedia.org/wiki/Courbe_d%27Aston))
+
+Les données proviennent Atomic Mass Data collaboration, disponible sur le site de l'Agence internationale de l'énergie atomique. Le fichier à utiliser est mass20.txt (les versions précédentes ne fonctionneront pas en raison du changement de format) : https://www-nds.iaea.org/amdc/
--- courbe_aston.py
+++ courbe_aston.py
@@ -1,39 +1,63 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 01 14:01:01 2022
+import matplotlib.pyplot
+import pandas
 
-@author: Séléna Laval
-selena.laval@u-bordeaux.fr
-laval.selena@gmail.com
-"""
+def main():
+    # Lecture du fichier
+    # Les colonnes sont définies manuellement
+    df = pandas.read_fwf(
+        "mass20.txt", 
+        skiprows=35, 
+        colspecs=[
+            (0,1),
+            (1, 4), 
+            (5, 9), 
+            (10, 14), 
+            (15,19), 
+            (20, 22), 
+            (23,27), 
+            (28, 42), 
+            (43, 55), 
+            (56,68), 
+            (69,78),
+            (79, 82),
+            (83, 93),
+            (94, 104),
+            (105, 123),
+            (124, 133)
+            ],
+            header=0,
+            names=[
+                "Fortran Caracter Control",
+                "N-Z",
+                "N",
+                "Z",
+                "A",
+                "EL",
+                "O",
+                "MASS EXCESS - Values",
+                "MASS EXCESS - Uncertainty",
+                "BINDING ENERGY - Values",
+                "BINDING ENERGY - Uncertainty",
+                "B",
+                "BETA-DECAY ENERGY - Values",
+                "BETA-DECAY ENERGY - Uncertainty",
+                "ATOMIC MASS - Values",
+                "ATOMIC MASS - Uncertainty"
+            ])
 
-import numpy as np;
-import pylab as plt;
-import re as re;
+    # On supprime les caractères # de la colonne BINDING ENERGY - Values et on transtype les données au format float
+    df['BINDING ENERGY - Values'] = df['BINDING ENERGY - Values'].map(lambda x: x.rstrip('#')) # type: ignore
+    df['BINDING ENERGY - Values'] = df['BINDING ENERGY - Values'].astype("Float64")
+    df['A'] = df['A'].astype("Float64")
 
-file = open("Aston.dat", "rt");
-
-liste_A = np.array([]);
-liste_energy = np.array([]);
-
-for i, line in enumerate(file):
-    #Expression régulière pour attraper les valeurs de A et de binding energy
-    pattern = re.compile(r'^[0-9-]*\s*[0-9-]+\s*[0-9-]+\s*[0-9-]+\s*([0-9-]+)\s*[a-zA-Z]+\s*[a-z0-9-]*\s*[0-9.#-]+\s*[0-9.#-]+\s*([0-9.-]+)#?');
-    finds = re.match(pattern, line);
+    # On trace le graphique x=A et y=BINDING ENERGY - Values
+    matplotlib.pyplot.plot(df["A"], df[["BINDING ENERGY - Values"]], 'r+')
+    matplotlib.pyplot.title("Courbe d'Aston")
+    matplotlib.pyplot.xlabel("A")
+    matplotlib.pyplot.ylabel("binding energy (KeV)")
+    matplotlib.pyplot.savefig("plot.png")
+    matplotlib.pyplot.show()
     
-    if finds and len(finds.groups()) == 2: #Si l'expression régulière trouve bien A et energy on ajoute au tableau
-        liste_A = np.append(liste_A, float(finds.group(1)));
-        liste_energy = np.append(liste_energy, float(finds.group(2)));
-        
-print(liste_A);
-print(liste_energy);
-
-assert len(liste_A) == len(liste_energy); #Au cas où les deux tableaux A et energy ne sont pas egaux on arrête
-
-#On trace le graphique
-plt.plot(liste_A, liste_energy, 'r+');
-plt.title('Courbe d\'Aston ');
-plt.xlabel('A')
-plt.ylabel('binding energy (KeV)')
-plt.savefig("plot.png");
-plt.show();
+if __name__ == "__main__":
+    main()
